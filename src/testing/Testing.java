e98f32f8b351bbdbc054fda924bd6cb6638651df
faruck06 <faruck06@gmail.com>
2016-06-07 19:18:18 -0500

    Update Testing.java

--- src/testing/Testing.java
+++ src/testing/Testing.java
@@ -15,7 +15,7 @@ public class Testing {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Faruck");
+        System.out.println("FaruckJJJJJ");
     }
     
 }
