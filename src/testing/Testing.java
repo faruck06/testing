b7c5b9147a0f4b20148a992cad22951270f936b7
Developer <Developer@10.0.2.15>
2016-06-07 19:33:49 -0500

    First Version

--- src/testing/Testing.java
+++ src/testing/Testing.java
@@ -15,7 +15,7 @@ public class Testing {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("FaruckJJJJJ");
+        System.out.println("Pedro");
     }
     
 }
